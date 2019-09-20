2483e1e432506be870300e8071f01cfcf1a6ba7b
David Vilaça <vilacapdavid@gmail.com>
2019-09-20 07:59:59 -0300

    feat: add morgan logger

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import express from 'express';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
+import morgan from 'morgan';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -24,6 +25,7 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(morgan('common'));
   }
 
   routes() {
